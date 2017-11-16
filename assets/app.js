ae4977cdc1ac7d7248d093ac451618f35c5dd402
Andrei <adyta094@gmail.com>
2017-11-16 11:29:49 +0200

    beautified code

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 // AndreiKnight
 // when document loaded
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let model = {
         height: 0,
         width: 0,
@@ -8,36 +8,36 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     let controller = {
-        init: function() {
+        init: function () {
             // let's get the current settings
 
             // height
             let height = document.querySelector("#height")
-            
+
             // width
-            let width  = document.querySelector("#width")
+            let width = document.querySelector("#width")
 
             // color
-            let color  = document.querySelector("#color")
+            let color = document.querySelector("#color")
 
-            color.addEventListener("change", function() {
-                
+            color.addEventListener("change", function () {
+
                 model.color = this.value
             })
             // init the view
             view.init()
         },
 
-        submit: function() {
+        submit: function () {
             // on click , update our model with the input's values
             model.height = height.value
-            model.width  = width.value
-            model.color  = color.value
+            model.width = width.value
+            model.color = color.value
 
             view.render()
         },
 
-        getSettings: function() {
+        getSettings: function () {
             let settings = {
                 height: model.height,
                 width: model.width,
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
             return settings
         },
 
-        resetSettings: function() {
+        resetSettings: function () {
             model.height = 0
             model.width = 0
             model.color = "#000"
@@ -55,29 +55,29 @@ document.addEventListener("DOMContentLoaded", function() {
             view.render();
         },
 
-        click_on_block: function(block) {
+        click_on_block: function (block) {
             let current_collor = controller.getSettings().color
             console.log(current_collor)
             block.style.backgroundColor = current_collor
 
         },
 
-        erase_block: function(block) {
+        erase_block: function (block) {
             block.style.backgroundColor = "#FFF"
         }
 
     }
 
     let view = {
-        init: function() {
+        init: function () {
             // select the grid
             let grid = document.querySelector("#grid")
-            
+
             // submit button
             let submit_btn = document.querySelector("#submit")
 
             // when we click run the controller.submit() function
-            submit_btn.addEventListener("click", function() {
+            submit_btn.addEventListener("click", function () {
                 // clear the grid
                 grid.innerHTML = ""
                 controller.submit()
@@ -87,29 +87,29 @@ document.addEventListener("DOMContentLoaded", function() {
             this.render()
         },
 
-        render: function() {
+        render: function () {
             // if we got these values, render the grid
-            
+
             if (controller.getSettings()) {
-                grid.style.gridTemplateColumns = "repeat("+controller.getSettings().width+", 20px)"
-                grid.style.gridTemplateRows = "repeat("+controller.getSettings().height+", 20px)"
-                
+                grid.style.gridTemplateColumns = "repeat(" + controller.getSettings().width + ", 20px)"
+                grid.style.gridTemplateRows = "repeat(" + controller.getSettings().height + ", 20px)"
+
 
                 let blocks = Number(controller.getSettings().height) * Number(controller.getSettings().width)
-                
+
                 for (let block = 0; block < blocks; block++) {
-                    
+
                     // create a block (pixel)
                     let block_div = document.createElement("div")
                     block_div.className = "block"
 
                     //paint
-                    block_div.addEventListener("click", function() {
+                    block_div.addEventListener("click", function () {
                         controller.click_on_block(this)
                     })
 
                     // erase pixel
-                    block_div.addEventListener("contextmenu", function(e) {
+                    block_div.addEventListener("contextmenu", function (e) {
                         e.preventDefault()
                         controller.erase_block(this)
                     })
